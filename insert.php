676c58830e33336babaa613caf0868c02035c4de
Davidfhk <davidign1688@hotmail.com>
2017-04-08 11:18:46 +0200

    comentarios añadidos

--- insert.php
+++ insert.php
@@ -1,12 +1,16 @@
 <?php
 try{
 	require "conexion.php";
-
+	
+// Guardamos en variable lo que nos llega por POST del formulario de index.php (para insertar un nuevo dato)
 	$name = $_POST['name'];
 	$apell = $_POST['surname'];
 	$direc = $_POST['address'];
 
+// Hacemos un insert con marcadores
 	$conexion = $pdo->prepare("INSERT INTO `datos_usuarios`(`nombre`, `apellido`, `direccion`) VALUES (:name,:apell,:direc)");
+
+// asignamos cada marcador a la variable correcta (que hemos creado al principio)
 	$conexion->bindValue(':name',$name);
 	$conexion->bindValue(':apell',$apell);
 	$conexion->bindValue(':direc',$direc);
